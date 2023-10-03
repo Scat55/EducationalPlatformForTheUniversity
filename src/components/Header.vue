92208223c8ebcf36b8f4f35dd81683249fcfe136
Dmitry Bystrov <dmit.bystroff2106@yandex.ru>
2023-10-03 11:13:28 +0300

    Добавил хедер

---
+++ src/components/Header.vue
@@ -0,0 +1,86 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+  <div class="header">
+    <div class="container">
+
+      <div class="header__logo">
+        <RouterLink to="/">
+          <img
+            src="../assets/images/ЛОГО3.png"
+            alt="Logo"
+            class="header__logo-img"
+          >
+        </RouterLink>
+        <div class="header__logo-lists">
+          <ul class="header__logo-list">
+            <li class="header__logo-item">Задания</li>
+            <li class="header__logo-item">О нас</li>
+            <li class="header__logo-item">Полезное</li>
+            <RouterLink
+              to="/reg"
+              class="router"
+            >
+              <li class="header__logo-item login">Войти</li>
+            </RouterLink>
+          </ul>
+        </div>
+
+      </div>
+
+    </div>
+  </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+@import '../assets/styles/styles.scss';
+
+.header {
+  color: $whiteColor;
+  margin-bottom: 3.125rem;
+
+  &__logo {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background-color: $accentColor;
+    padding: 0 3rem;
+    border-bottom-left-radius: 1rem;
+    border-bottom-right-radius: 1rem;
+
+
+    &-img {
+      width: 4.3rem;
+      margin-top: .625rem;
+      margin-bottom: 1rem;
+    }
+
+    &-list {
+      display: flex;
+      align-items: center;
+      gap: 1.25rem;
+    }
+
+    &-item {
+      list-style: none;
+      cursor: pointer;
+      transition: all .3s;
+
+      &:hover {
+        transform: scale(1.1);
+      }
+    }
+  }
+}
+
+.login {
+  color: $whiteColor;
+  background-color: $lightBlueColor;
+  padding: .625rem 1.25rem;
+  border-radius: 1rem;
+}
+</style>
---
+++ src/views/RegPage.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="reg">
+    <div class="container">
+      dndfjdfj
+    </div>
+  </div>
+</template>
+
+
+
+<style lang="scss" scoped></style>
