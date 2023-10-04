2d305a28cbcf638cc859c1a32c0e3aba1b8b1397
Dmitry Bystrov <dmit.bystroff2106@yandex.ru>
2023-10-04 11:31:20 +0300

    Изменил текст

--- src/components/Computer.vue
+++ src/components/Computer.vue
@@ -12,7 +12,9 @@
           alt="Computer"
           class="computer__img"
         >
-        <p class="computer__title">Уроки, тесты, проекты по школьной программе<span class="computer__subtitle">Смотреть
+        <p class="computer__title">Дистанционное обучение открывает перед вами новые горизонты. Благодаря нашим вебинарам
+          и видеоурокам, вы можете учиться у лучших специалистов, не выходя из дома. Доступ к знаниям 24/7 в любой точке
+          мира.<span class="computer__subtitle">Смотреть
             задания -></span></p>
 
       </div>
@@ -40,13 +42,13 @@
   }
 
   &__title {
-    font-size: 3rem;
+    font-size: 1.4rem;
   }
 
   &__subtitle {
     display: block;
     padding-top: 1rem;
-    font-size: 1.6rem;
+    font-size: 1.1rem;
     cursor: pointer;
     transition: all .3s;
 
